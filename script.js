1fd2bbd8aa3fd42936b982633b3da8c500db307a
maceomanish0819 <manishmaceo7@gmail.com>
2026-02-13 22:29:40 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesButton").addEventListener("click", function(){
+    alert("Yay!! I can't wait to spend my valentine's day with you Babe! Aailoveyouusooomuchhhmysweetie! halka emoji halnu ayena");
+});
+
+document.getElementById("noButton").addEventListener("click", function(){
+    alert("Please say Yes my darling! Say Yes My love");
+});
